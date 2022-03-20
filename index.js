c4fb22bb4811d6f6d4a39173f7329aba59cf0fdc
Pratik Borole <borolepratik@gmail.com>
2022-03-20 12:55:04 -0700

    First stab at handling raw commands

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.data.name, command);
 }
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-
 for (const file of eventFiles) {
 	const event = require(`./events/${file}`);
 	if (event.once) {
---
+++ raw-commands/gfc.js
@@ -0,0 +1,3 @@
+module.exports = async (message) => {
+	await message.reply(`Hey〈〈${message.author} 〉〉! The current commands are "gfc", "gfc-guess", "gfc-help"`);
+};
