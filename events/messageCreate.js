775023620fe888ebe8135845a6f659082f9a47f0
Pratik Borole <borolepratik@gmail.com>
2022-03-20 16:44:39 -0700

    Added gfc-help command

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,30 +1,31 @@
 const fs = require('node:fs');
 const { Collection } = require('discord.js');
 
-const rawCommandFiles = fs.readdirSync('./raw-commands').filter(file => file.endsWith('.js'));
-const rawCommands = new Collection();
-for (const file of rawCommandFiles) {
-	const rawCommand = require(`../raw-commands/${file}`);
-	const fileName = file.replace('.js', '');
-	rawCommands.set(fileName, rawCommand);
-	// console.log(`file name = ${fileName}`);
-}
+const generateRawCommands = () => {
+	const rawCommandFiles = fs.readdirSync('./raw-commands').filter(file => file.endsWith('.js'));
+	const rawCommands = new Collection();
+	for (const file of rawCommandFiles) {
+		const rawCommand = require(`../raw-commands/${file}`);
+		const fileName = file.replace('.js', '');
+		rawCommands.set(fileName, rawCommand);
+	}
+	return rawCommands;
+};
 
 module.exports = {
 	name: 'messageCreate',
 	once: false,
 	execute(message) {
-		console.log(`Message created - ${message}`);
 		if (!message.author.bot && message.channel.name === 'slasherbot') {
-			// message.reply('bleh');
 			const args = message.content.split(' ');
 
 			if (args.length == 0 || args[0].charAt(0) !== '!') return;
 
 			const command = args.shift().substr(1);
-			if (Object.keys(rawCommands).includes(command)) {
-				rawCommands[command](message);
-			}
+			const rCommands = generateRawCommands();
+
+			const commandMatch = rCommands.get(command);
+			if (commandMatch) commandMatch(message);
 		}
 	},
 };
---
+++ raw-commands/gfc-help.js
@@ -0,0 +1,3 @@
+module.exports = async (message) => {
+	await message.reply(`Hey there〈〈${message.author} 〉〉! You just ran [COMMAND:gfc-help] Right now some bad ass gfc community member needs to get this command implemented! Maybe that dope person is you?`);
+};
