d28fbc38e0aa09a3cd68be9e02be0ac49a7c9ff0
Pratik Borole <borolepratik@gmail.com>
2022-03-20 12:55:04 -0700

    First stab at handling raw commands

---
+++ raw-commands/gfc-guess.js
@@ -0,0 +1,16 @@
+const guess = [
+	'As I see it, yes.',
+	'Ask again later.',
+	'Better not tell you now.',
+	'Cannot predict now.',
+	'Concentrate and ask again.',
+	'Don\'t count on it.',
+	'It is certain.',
+	'It is decidedly so.',
+];
+
+module.exports = async (message) => {
+	const i = Math.floor(Math.random() * guess.length);
+	const reply = guess[i];
+	await message.reply(`〈〈${message.author}〉〉⌨︎ ${reply}`);
+};
