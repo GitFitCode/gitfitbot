e801596a8fde7837d1757054e0920dbba91b5e6c
Pratik Borole <borolepratik@gmail.com>
2022-09-05 12:33:56 -0700

    feat: added `requestor` and `discord channel id` to support ticket props

--- src/utils/notion.ts
+++ src/utils/notion.ts
@@ -5,7 +5,9 @@ const _notion = new Client({ auth: process.env.NOTION_KEY ?? "" });
 const _databaseId = process.env.NOTION_DATABASE_ID ?? "";
 
 async function createNotionDBEntry(
-  issueText: string | number | boolean | undefined
+  issueText: string | number | boolean | undefined,
+  authorUsername: string,
+  channelID: string
 ): Promise<string> {
   try {
     const response = await _notion.pages.create({
@@ -20,10 +22,29 @@ async function createNotionDBEntry(
             },
           ],
         },
+        Requestor: {
+          rich_text: [
+            {
+              text: {
+                content: authorUsername,
+              },
+            },
+          ],
+        },
+        "Discord Channel ID": {
+          rich_text: [
+            {
+              text: {
+                content: channelID,
+              },
+            },
+          ],
+        },
       },
     });
     return response.id;
   } catch (error: any) {
+    // https://github.com/makenotion/notion-sdk-js#handling-errors
     console.error(error.body);
     return "";
   }
@@ -41,6 +62,7 @@ async function updateNotionDBEntry(pageID: string) {
       },
     });
   } catch (error: any) {
+    // https://github.com/makenotion/notion-sdk-js#handling-errors
     console.error(error.body);
   }
 }
