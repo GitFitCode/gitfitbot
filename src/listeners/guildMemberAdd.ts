ad807adaf5fb1afc6bf7f6f5c113d72b5b12874d
Pratik Borole <borolepratik@gmail.com>
2023-03-06 18:03:56 -0800

    chore: updated DM text sent to user on joining the server

--- src/listeners/guildMemberAdd.ts
+++ src/listeners/guildMemberAdd.ts
@@ -13,7 +13,7 @@ export default (client: Client): void => {
 Hey @everyone, <@${member.id}> has joined the community! Give them a warm welcome and introduce yourselves when you get the chance. 😄`;
     const channel: any = client.channels.cache.get(channelID);
 
-    // Send a message to the General channel.g
+    // Send a message to the General channel.
     channel.send(channelMessage);
 
     const GFCIntroSurveyLink = config.gfcIntroSurveyLink;
@@ -21,11 +21,18 @@ Hey @everyone, <@${member.id}> has joined the community! Give them a warm welcom
     const Admin2ID = config.admin2DiscordId;
     const userMessage = `Hey <@${member.id}>, welcome to **GitFitCode**!
 
-Before you jump in, can you quickly fill this out: ${GFCIntroSurveyLink}.
+Before you jump in, can you quickly fill this out: ${GFCIntroSurveyLink}?
 
-Here you will find a group of like-minded life long learners, channels to nerd out about your favorite technologies, help each other out, share progress in your development journey, and to have a good time making this a home for yourself!  We are happy you are here with us! Feel free to get to know the other members of the server as well. Head over to the general channel and give a quick intro about who you are, what you like to do in your free time, your favorite tech stack and tools, etc. We want to get to know you.
+In GitFitCode, you will find a group of like-minded life-long learners, channels to nerd out about your favorite topics, help each other out, share progress in your development journey, and to have a good time making this a home for yourself!
+
+**We are happy you are here with us**!
+
+Feel free to get to know the other members of the server as well. Head over to the general channel and give a quick intro about who you are, what you like to do in your free time, your favorite tech stack and tools, etc. We want to get to know you!
 
 If you have any questions or concerns please don’t hesitate to reach out to **<@${Admin1ID}>** or **<@${Admin2ID}>**.
+
+Please also ensure to update your **GitFitCode** server profile (nickname field) with your name (and optionally add your timezone abbreviation) so it is easier for the community to address you.
+Note that updating the name in the server profile **does not** require a Nitro subscription. More info here - https://support.discord.com/hc/en-us/articles/4409388345495-Server-Profiles#h_01FGCPPE37QG92W7XCVV53NVC3.
 
 Looking forward to seeing your positive impact within our community! 😊`;
 
